8d40829cc6bcb8f9a23d1ec987011a64e26590ab
wlz <wanglizong@meizu.com>
2017-09-19 16:19:07 +0800

    修改实体属性

--- src/main/java/com/demo/entity/UserBookEntity.java
+++ src/main/java/com/demo/entity/UserBookEntity.java
@@ -6,5 +6,39 @@ import java.io.Serializable;
  * Created by Administrator on 2017-09-19.
  */
 public class UserBookEntity implements Serializable {
+	
+	private static final long serialVersionUID = 4599543678350818379L;
+
+	public Integer id;
+	
+	public Integer bookId;
+	
+	public Integer userId;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(Integer bookId) {
+		this.bookId = bookId;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	
+	
 
 }
