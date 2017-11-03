9b683b9c53c12bc9bde59ad3996c42a46edc20fe
wlz <wanglizong@meizu.com>
2017-11-03 17:57:57 +0800

    修复评论

--- src/main/java/com/demo/dto/IdEntity.java
+++ src/main/java/com/demo/dto/IdEntity.java
@@ -1,8 +1,12 @@
 package com.demo.dto;
 
+import java.util.Date;
+
 public class IdEntity {
 	
 	public Integer id ;
+	
+	public Date createTime;
 
 	public Integer getId() {
 		return id;
@@ -10,6 +14,14 @@ public class IdEntity {
 
 	public void setId(Integer id) {
 		this.id = id;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
 	}
 	
 
